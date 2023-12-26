adf70a8095434bddf069f8b6ba9c3fb52361853d
Ekaterina <i.am.catherine@yandex.ru>
2023-12-26 03:05:48 +0300

    Add 11

---
+++ labs/Lab_11_REACT_difficult_easy/Solution/components/Header/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const DivWrapper = styled.div`
+    background-color: #2a2b2d;
+    height: 36px;
+`;
+
+const Header = ()=>{
+    return(
+        <DivWrapper/>
+    );
+}
+
+export { Header };
